33fc3e22dd44e250a8bb80542dde6b2bca1f87ae
Jhon Baron <jhooomn@gmail.com>
2023-05-22 16:10:21 -0500

    login working properly

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,7 +4,8 @@ import { Card } from "primereact/card";
 import { Password } from "primereact/password";
 import { InputText } from "primereact/inputtext";
 import { useNavigate } from "react-router-dom";
-
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 import { Button } from "primereact/button";
 
 export type LoginProps = {
@@ -14,12 +15,44 @@ export type LoginProps = {
 const Login: React.FC<LoginProps> = () => {
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const MySwal = withReactContent(Swal);
   const handleSubmit = (event: any) => {
     event.preventDefault();
-    navigate("/home");
+    fetch("https://heart-guardian-service.vercel.app/users/login", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user: {
+          email: email,
+          password: password,
+        },
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        navigate("/home");
+      })
+      .catch((error) => {
+        error.then((errorMessage: any) => {
+          MySwal.fire({
+            position: "center",
+            icon: "error",
+            title: errorMessage,
+            showConfirmButton: true,
+            timer: 2500,
+          });
+        });
+      });
+    //
   };
 
   const handleSignUp = (event: any) => {
@@ -35,10 +68,11 @@ const Login: React.FC<LoginProps> = () => {
       <form onSubmit={handleSubmit}>
         <div className="flex justify-content-center">
           <InputText
-            value={username}
-            placeholder="Usuario"
-            onChange={(e) => setUsername(e.target.value)}
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
             className="login-inputs"
+            type="email"
           />
         </div>
         <div className="flex justify-content-center">
@@ -53,7 +87,11 @@ const Login: React.FC<LoginProps> = () => {
         <Button type="submit" label="Iniciar Sesion" className="login-inputs" />
       </form>
       <div className="flex justify-content-center">
-        <Button label="Registrarse" onClick={handleSignUp} className="login-inputs" />
+        <Button
+          label="Registrarse"
+          onClick={handleSignUp}
+          className="login-inputs"
+        />
       </div>
     </Card>
   );
